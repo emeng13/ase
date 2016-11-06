d73a6b21a164c9f78fd3e12fd089cc4e787e5563
Emily Meng <ewm2136@columbia.edu>
2016-11-06 15:10:29 -0500

    tests for adding and removing items from bill

--- test.py
+++ test.py
@@ -1,48 +1,70 @@
-"""Shopping list unit tests"""
+"""app unit tests"""
 # reference: http://www.drdobbs.com/testing/unit-testing-with-python/240165163
 import unittest
+import pymssql 
 
-from shopping_list import app, db
-from shopping_list import shopping_list
+from app import app
+
+user_email = "test@test.com"
+bill_id = -1
 
 class MyTest(unittest.TestCase):
 	"""Test class"""
 	def setUp(self):
 		"""Set up"""
-		app.config['TESTING'] = True
-		app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-		self.app = app.test_client()
-		db.create_all()
+
 
 	def tearDown(self):
 		"""Tear down"""
-		db.session.remove()
-		db.drop_all()
 
+	
 	def test_add_item(self):
-		"""Add item to list"""
-		l1 = shopping_list(name='test1', quantity='1')
-		db.session.add(l1)
-		db.session.commit()
+		"""Add item to bill"""
+		global user_email
+		global bill_id
 
-		assert l1 in db.session
+		item_name = "test_add_item"
+		quantity = 3
+		price = 1.50
+
+		cursor = conn.cursor()
+		cursor.execute("INSERT INTO Items VALUES (%s, %s, %d, %d, %d)", (user_email, item_name, quantity, price, bill_id))
+    	conn.commit()
+
+		cursor = conn.cursor()
+		cursor.execute("SELECT * FROM Items WHERE Email=%s AND ItemName=%s AND Quantity=%d AND Price=%d AND BillId=%d", (user_email, item_name, quantity, price, bill_id))
+		
+		assert cursor.rowcount == 1
+
 
 	def test_remove_item(self):
 		"""Add then remove item from list"""
-		l2 = shopping_list(name='test2', quantity='2')
-		db.session.add(l2)
-		db.session.commit()
+		global user_email
+		global bill_id
 
-		assert l2 in db.session
+		item_name = "test_remove_item"
+		quantity = 5
+		price = 1.00
 
-		db.session.delete(l2)
-		db.session.commit()
+		cursor = conn.cursor()
+		cursor.execute("INSERT INTO Items VALUES (%s, %s, %d, %d, %d)", (user_email, item_name, quantity, price, bill_id))
+    	conn.commit()
 
-		testname = 'test2'
+		cursor = conn.cursor()
+		cursor.execute("SELECT * FROM Items WHERE Email=%s AND ItemName=%s AND Quantity=%d AND Price=%d AND BillId=%d", (user_email, item_name, quantity, price, bill_id))
+		
+		assert cursor.rowcount == 1
 
-		q = shopping_list.query.filter_by(name=testname).first()
+		cursor = conn.cursor()
+		cursor.execute("DELETE FROM Items WHERE Email=%s AND ItemName=%s AND Quantity=%d AND Price=%d AND BillId=%d", (user_email, item_name, quantity, price, bill_id))
+		conn.commit()
 
-		assert q is None
+		cursor = conn.cursor()
+		cursor.execute("SELECT * FROM Items WHERE Email=%s AND ItemName=%s AND Quantity=%d AND Price=%d AND BillId=%d", (user_email, item_name, quantity, price, bill_id))
+		
+		assert cursor.rowcount == 0
+
+	def test
 
 
 if __name__ == '__main__':
