78892b7edfd6d44d6493f8a10270952f57bad7ad
Sherry Qiu <sherryq6@gmail.com>
2016-11-12 16:35:20 -0500

    login and signup tests

--- test.py
+++ test.py
@@ -24,20 +24,34 @@ class MyTest(unittest.TestCase):
 	def tearDown(self):
 	   """Tear down"""
 
-	
-	def test_add_item(self):
-	   """Add item to bill"""
-
-
-	def test_remove_item(self):
-	   """Add then remove item from list"""
-
 	def test_split_cost(self):
 	   #rv = self.app.post('/split_cost?Tip=0.2&Billid=364&Total=2.20')
 	   # rv = self.app.post('/split_cost', data={'Tip': '0.2', 'Billid': '364', \
 	   #  'Total': '2.20'}, follow_redirects=True)
-	   rv = self.app.post('/split_cost', data=dict(Tip= '0.2', Billid= '364', Total= '2.20'),follow_redirects=True)
+	   rv = self.app.post('/split_cost', data=dict(Tip= '0.2', Billid= '364', Total= '2.20'), follow_redirects=True)
 	   assert '2.64' in rv.data
+
+	def test_login_successful(self):
+		rv = self.app.post('/login', data={'email': "test@test", 'password': 'test'}, follow_redirects=True)
+		assert 'Bill' in rv.data
+
+	def test_login_user_not_exist(self):
+		rv = self.app.post('/login', data={'email': "not_test@test", 'password': 'test'}, follow_redirects=True)
+		assert 'Sign up first' in rv.data
+
+	def test_login_incorrect_password(self):
+		rv = self.app.post('/login', data={'email': "test@test", 'password': 'not_test'}, follow_redirects=True)
+		assert 'Incorrect password' in rv.data
+
+	def test_signup_successful(self):
+		rv = self.app.post('/signUp', data={'firstName': 'Test', 'lastName': 'Test', 'email': "test1@test", \
+			'password': 'test'}, follow_redirects=True)
+		assert 'Your account is registered successfully!' in rv.data
+
+	def test_signup_same_user(self):
+		rv = self.app.post('/signUp', data={'firstName': 'Test', 'lastName': 'Test', 'email': "test@test", \
+			'password': 'test'}, follow_redirects=True)		
+		assert 'You registered with the same email before.' in rv.data
 
 
 
